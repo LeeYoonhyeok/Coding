6210599e7d45766a1fcc1c1cad4462d7439fe60b
GH-00 <ghkim7319@gmail.com>
2024-06-17 22:27:00 +0900

    [add]연산자 끼워넣기 - 김가현

--- Algorithms/DFSBFS/연산자끼워넣기/김가현.py
+++ Algorithms/DFSBFS/연산자끼워넣기/김가현.py
@@ -3,7 +3,7 @@ input = sys.stdin.readline
 
 n = int(input())
 num = list(map(int, input().split()))
-op = list(map(int, input().split())) # +, -, *, //
+op = list(map(int, input().split()))  # +, -, *, //
 
 max_n = -1e9
 min_n = 1e9
@@ -13,7 +13,7 @@ def dfs(level, total, plus, minus, mult, div):
     if level == n:
         max_n = max(total, max_n)
         min_n = min(total, min_n)
-        return 
+        return
     
     if plus:
         dfs(level + 1, total + num[level], plus - 1, minus, mult, div)
@@ -22,8 +22,10 @@ def dfs(level, total, plus, minus, mult, div):
     if mult:
         dfs(level + 1, total * num[level], plus, minus, mult - 1, div)
     if div:
-        dfs(level + 1, int(total / num[level]), plus, minus, mult, div - 1)
-
+        if total < 0:
+            dfs(level + 1, -(-total // num[level]), plus, minus, mult, div - 1)
+        else:
+            dfs(level + 1, total // num[level], plus, minus, mult, div - 1)
 
 dfs(1, num[0], op[0], op[1], op[2], op[3])
 print(max_n)
