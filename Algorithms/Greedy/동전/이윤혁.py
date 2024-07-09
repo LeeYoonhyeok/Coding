59a01353a0c26ee4f6b8a42547ec6413a0e3735b
LeeYoonhyeok <yoonhyeok321@gmail.com>
2024-07-09 22:00:33 +0900

    [Add] 동전_이윤혁

---
+++ Algorithms/Greedy/동전/이윤혁.py
@@ -0,0 +1,9 @@
+import sys
+N, K = map(int, sys.stdin.readline().split())
+coins = [int(sys.stdin.readline()) for _ in range(N)]
+cnt = 0
+for coin in coins[::-1]:
+    if K >= coin:
+        cnt += K // coin
+        K %= coin
+print(cnt)
