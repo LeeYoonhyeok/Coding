857c40dfaca1048e03c23545bf88ed961a3f5756
nanji625 <160685081+nanji625@users.noreply.github.com>
2024-08-31 15:47:32 +0900

    Update 남지성.py

--- 실전코테대비/메뉴리뉴얼_kakao/남지성.py
+++ 실전코테대비/메뉴리뉴얼_kakao/남지성.py
@@ -1,2 +1,23 @@
+from itertools import combinations
+from collections import Counter
 
-ㄴㅇㄹ
+def solution(orders, course):
+    result = []
+    
+    for course_len in course:
+        combs = []
+        
+        for order in orders:
+            combs.extend(combinations(sorted(order), course_len))
+        
+        combs_count = Counter(combs)
+        
+        if combs_count:
+            max_count = max(combs_count.values())
+            if max_count > 1:
+                for comb, count in combs_count.items():
+                    if count == max_count:
+                        result.append(''.join(comb))
+    
+    return sorted(result)
+                
