ef1fa516c8d9640b06214b5b8bba171dba7077b7
Lee Yoonhyeok <yoonhyeok321@gmail.com>
2024-10-12 23:38:09 +0900

    add: 평범한배낭 python code

--- Algorithms/DP/평범한배낭/이윤혁.py
+++ Algorithms/DP/평범한배낭/이윤혁.py
@@ -1,16 +1,24 @@
 '''
-
+dp문제. 배낭에 넣을 수 있는 최대의 물건의 가치 구하기
+dp테이블에 최대 무게인 K부터 순회하고 있는 item의 weight까지 for문을 돌며 해당 구간의 dp마다 현재 or 물건을 넣었을 때를 비교하며 계산
 '''
-N, K = map(int, input().split())  # 물건의 개수, 배낭의 최대 무게
-items = [list(map(int, input().split())) for _ in range(N)]  # 각 물건의 무게와 가치
+N, K = map(int, input().split())  # N: 물건의 개수, K: 배낭의 최대 무게
+items = [list(map(int, input().split())) for _ in range(N)] # 각 물건의 무게와 가치
 
-# DP 배열 초기화 (1차원 배열 사용)
 dp = [0] * (K + 1)
-
-# DP 계산
 for weight, value in items:
-    for w in range(K, weight - 1, -1):
-        dp[w] = max(dp[w], dp[w - weight] + value)
-
-# 결과 출력
+    for w in range(K, weight-1, -1):
+        dp[w] = max(dp[w], dp[w-weight] + value)
 print(dp[K])
+
+
+
+
+
+
+
+
+
+
+
+
